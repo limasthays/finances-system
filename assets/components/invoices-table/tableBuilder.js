e6f3b729d5eab61af7e2a84a55b638f0d7d6a270
User <limasthays@gmail.com>
2023-12-19 02:30:35 -0300

    changing titles in table data

--- assets/components/invoices-table/tableBuilder.js
+++ assets/components/invoices-table/tableBuilder.js
@@ -5,14 +5,14 @@ function tableBuilder(arrayData){
 	arrayData.forEach(item => {
 		table.append(
 			`<tr class="invoice-item">
-			<th scope="row" title="id da nota fiscal">${item.id}</th>
-			<td class="text-capitalize payer" title="pagador">${item.pagador}</td>
-			<td title="valor total">R$ ${item.valor}</td>
-			<td class="status" title="status da nota fiscal">${item.status}</td>
-			<td class="emission-date" title="data de emissão">${item.emissao}</td>
-			<td class="charge-date" title="data de cobrança">${item.cobranca}</td>
-			<td title="data de vencimento">${item.vencimento}</td>
-			<td class="payment-date" title="data de pagamento">${item.pagamento}</td>
+			<th scope="row" title="Id da nota fiscal">${item.id}</th>
+			<td class="text-capitalize payer" title="Pagador">${item.pagador}</td>
+			<td title="Valor da nota fiscal">R$ ${item.valor}</td>
+			<td class="status" title="Status da nota fiscal">${item.status}</td>
+			<td class="emission-date" title="Data de emissão">${item.emissao}</td>
+			<td class="charge-date" title="Data de cobrança">${item.cobranca}</td>
+			<td title="Data de vencimento">${item.vencimento}</td>
+			<td class="payment-date" title="Data de pagamento">${item.pagamento}</td>
 			<td>
 				<button class="btn btn-outline-info btn-sm text-uppercase copy-access-key" title="Clique para copiar a chave de acesso" name="${item.chave_de_acesso}">
 					<i class="bi bi-clipboard-check-fill"></i>
@@ -30,4 +30,5 @@ tableBuilder(data)
 $(".copy-access-key").on("click", function(){
 	const accessKey = $(this).attr("name")
 	navigator.clipboard.writeText(accessKey)
+	alert('Chave de acesso copiada! Clique no botão "boleto" ou "nota fiscal" para acessar a nota fiscal.')
 })
