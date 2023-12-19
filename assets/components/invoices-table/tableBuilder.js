ef9d2781b9416ae25124d69c0ed854fc4751b163
User <limasthays@gmail.com>
2023-12-18 23:25:10 -0300

    final styles updates

--- assets/components/invoices-table/tableBuilder.js
+++ assets/components/invoices-table/tableBuilder.js
@@ -5,7 +5,7 @@ function tableBuilder(arrayData){
 			`<tr class="invoice-item">
 			<th scope="row" title="id da nota fiscal">${item.id}</th>
 			<td class="text-capitalize payer" title="pagador">${item.pagador}</td>
-			<td title="valor total">${item.valor}</td>
+			<td title="valor total">R$ ${item.valor}</td>
 			<td class="status" title="status da nota fiscal">${item.status}</td>
 			<td class="emission-date" title="data de emissão">${item.emissao}</td>
 			<td class="charge-date" title="data de cobrança">${item.cobranca}</td>
